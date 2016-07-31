cae2e7d9809560f6f19a1983691164a85c8688e8
VolokhDenis <davoloh@gmail.com>
2016-07-31 22:08:43 +0300

    edit buttons

--- src/js/header.js
+++ src/js/header.js
@@ -1,13 +1,17 @@
 // A $( document ).ready() block.
-$( document ).ready(function() {
-    console.log( "ready!" );
-});
+//$( document ).ready(function() {
+//    console.log( "ready!" );
+//});
 
-$('#addit ul li').mouseover(function(){
- 	$(this).stop().animate({backgroundColor: '#400101'});
- 	 console.log( "colorIn" );
-});
-$('#addit ul li').mouseout(function(){
- 	$(this).stop().animate({backgroundColor: 'red'});
- 	 console.log( "colorIn" );
-});
+//$('#addit ul li').mouseover(function(){
+//	Class_check=$(this).attr("class");
+// 	$(this).stop().animate({backgroundColor: '#400101'});
+
+//});
+//$('#addit ul li').mouseout(function(){
+//	if(Class_check){
+// 	$(this).stop().css({backgroundColor : "red"});
+// }else{
+// 	$(this).stop().css({'background-color' : 'rgba( 0, 0, 0, 0 )'});
+// }
+//});
